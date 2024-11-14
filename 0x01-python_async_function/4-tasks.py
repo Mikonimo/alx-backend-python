e18f46c7179cde86e2bcdbe770f4529a30f69125
Mark Manani <mikonani413@gmail.com>
2024-11-14 18:19:37 +0300

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python
+"""Contains a async function"""
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Take the code from wait_n and alter it into a new function"""
+    futures = [task_wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
+    return delays
